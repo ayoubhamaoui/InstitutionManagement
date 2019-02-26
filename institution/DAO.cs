fe43445ad62d5407e95492445197888e6021597f
ayoub <ayoub10hamaoui@gmail.com>
2019-02-26 02:17:43 +0100

    implementation of constructor | class DAO

--- institution/DAO.cs
+++ institution/DAO.cs
@@ -7,12 +7,18 @@ namespace institution
 {
     public class DAO : MySql
     {
-        public string[] param_co;
+        public static string[] param_co;
         private string Table;
 
-        public DAO()
+        public DAO(string BD, string Pass, string User, string Server, string Port, string Table) : base(BD, Pass, User, Server,Port)
         {
-            throw new System.NotImplementedException();
+            this.Table = Table;
+            param_co = new string[5];
+            param_co[0] = BD;
+            param_co[1] = Pass;
+            param_co[2] = User;
+            param_co[3] = Server;
+            param_co[4] = Port;
         }
 
         public List<Dictionary<string, string>> Select()
